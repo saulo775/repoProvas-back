2a156f42260fe92e6f8ab87e462cd0669d13b8c1
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-07-22 17:00:02 -0300

    TEST - refactoring auth tests.

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -1,12 +1,17 @@
 import supertest from "supertest";
 import app from "../src/app.js";
+import prisma from "../src/config/database.js";
 
-const EMAIL = `${new Date().getTime()}@gmail.com`;
+const EMAIL = `topuser@gmail.com`;
 const PASSWORD = "senha1234";
 const LOGIN = {email: EMAIL, password:PASSWORD, confirmPassword: PASSWORD};
 
 
 describe("auth test suite", ()=>{
+    beforeEach(async ()=>{
+        await prisma.$executeRaw`DELETE FROM users WHERE email=${EMAIL}`;
+    })
+
     it("should be able create user, given email, password and passwordConfirmation.", async ()=>{
         const response = await supertest(app).post("/sign-up").send(LOGIN);
         expect(response.statusCode).toBe(201);
@@ -17,14 +22,19 @@ describe("auth test suite", ()=>{
         expect(response.statusCode).toBe(422);
     });
 
+    it("given email already in use, fail to create user", async()=>{
+        await supertest(app).post("/sign-up").send(LOGIN);
+        const response = await supertest(app).post("/sign-up").send(LOGIN);
+        expect(response.statusCode).toBe(409);
+    });
+
     it("should be able to return a token given email and password", async ()=> {
         const response = await supertest(app).post("/sign-in").send({EMAIL, PASSWORD});
         const token = response.body.token;
         expect(token).not.toBeNull();
     });
+});
 
-    it("given email already in use, fail to create user", async()=>{
-        const response = await supertest(app).post("/sign-up").send(LOGIN);
-        expect(response.statusCode).toBe(409);
-    });
+afterAll(async ()=>{
+    await prisma.$disconnect();
 })
